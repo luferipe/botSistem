9b2922dcc9eb37564385ab1686f24fd418b257a0
macpro <613738>
2021-04-30 17:53:41 -0300

    Atualização

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,4 +1,4 @@
 <footer class="footer d-flex flex-column flex-md-row align-items-center justify-content-between">
-  <p class="text-muted text-center text-md-left">Copyright © 2021 <a href="https://www.nobleui.com" target="_blank">BotSistem</a>. Todos os direitos reservados</p>
+  <p class="text-muted text-center text-md-left">Copyright © 2021 <a href="https://www.blogfacil.com.br" target="_blank">Blogfacil.com.br</a>. Todos os direitos reservados</p>
   <p class="text-muted text-center text-md-left mb-0 d-none d-md-block">Desenvolvido com muito <i class="mb-1 text-primary ml-1 icon-small" data-feather="heart"></i></p>
 </footer>
