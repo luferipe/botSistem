b6a475a1ce1d18aba7284d29926c6f813023ed04
macpro <613738>
2021-05-01 19:32:48 -0300

    Rotas 3

--- resources/views/pages/users/create.blade.php
+++ resources/views/pages/users/create.blade.php
@@ -32,10 +32,32 @@
                                 <label for="name">Nome Completo</label>
                                 <input id="name" class="form-control" name="name" type="text">
                             </div>
+
                             <div class="form-group col-6 float-left">
                                 <label for="email">Email</label>
                                 <input id="email" class="form-control" name="email" type="email">
                             </div>
+
+                            <div class="form-group col-6 float-left">
+                                <label for="phone">Celular</label>
+                                <input id="phone" class="form-control" name="phone" type="tel">
+                            </div>
+
+                            <div class="form-group col-6 float-left">
+                                <label for="email">Email</label>
+                                <input id="email" class="form-control" name="email" type="email">
+                            </div>
+
+                            <div class="form-group col-6 float-left">
+                                <label for="phone">Celular</label>
+                                <input id="phone" class="form-control" name="phone" type="tel">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="password">Senha</label>
+                                <input id="password" class="form-control" name="password" type="password">
+                            </div>
+
                             <div class="form-group col-6 float-left">
                                 <label for="phone">Celular</label>
                                 <input id="phone" class="form-control" name="phone" type="tel">
